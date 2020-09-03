f79759b5c393e3c1e4884c2ec5fcbbc9644e0ca2
Bruno Virgilio de Lima <bvdelima@gmail.com>
2020-09-03 08:07:26 -0300

    Executando Parte3

--- modulo.py
+++ modulo.py
@@ -2,8 +2,11 @@ import random
 import time
 
 class Lutador:
-    def __init__(self,*n):
-        self.nome = n
+    def __init__(self,n1=0,n2=0 ,p1=0 ,p2=0):
+        self.nome1 = n1
+        self.nome2 = n2
+        self.ponto1 = p1
+        self.ponto2 = p2
         
     def setNome(self):
         j1=int(input(''' \t
@@ -26,33 +29,35 @@ class Lutador:
         3 - Junior Cigano
         Digite Aqui>>> '''))
         if j1 == 1:
-            self.nome = 'Anderson Silva'    
+            self.nome1 = 'Anderson Silva'    
         elif j1 == 2:
-            self.nome = 'José Aldo'
+            self.nome1 = 'José Aldo'
             
         elif j1 == 3:
-            self.nome = 'Junior Cigano'
+            self.nome1 = 'Junior Cigano'
 
-        print(f' Seu lutador é {self.nome}.')
+        print(f' Seu lutador é {self.nome1}.')
 
     def setNome2(self):
         l = ['Jon Jones','George St-Pierre','Chuck Liddell']
-        self.nome = random.choice(l)
-        print(f' O computador escolheu o {self.nome}.')
+        self.nome2 = random.choice(l)
+        print(f' O computador escolheu o {self.nome2}.')
         inicio = input('Tecle ENTER para iniciar a luta.')
 
     def Golpe1(self):
-        r = ['ACERTOU','ERROU']
-        resultado = random.choice(r)
         g1 = int(input('''
         Sua vez de jogar, escolha seu golpe:
         1 - soco
         2 - chute
         Digite Aqui>>> '''))
+        r = ['ACERTOU','ERROU']
+        resultado = random.choice(r)
+        if resultado == 'ACERTOU':
+            self.ponto1 += 1
         if g1 == 1:
-            print(f'{self.nome} aplicou um SOCO e {resultado}')
+            print(f'{self.nome1} aplicou um SOCO e {resultado}')
         else:
-            print(f'{self.nome} aplicou um CHUTE e {resultado}')
+            print(f'{self.nome1} aplicou um CHUTE e {resultado}')
 
     def Golpe2(self):
         print('Computador vai jogar...'),time.sleep(4)
@@ -60,7 +65,30 @@ class Lutador:
         g3 = random.choice(g2)
         r2 = ['ACERTOU','ERROU']
         resultado2 = random.choice(r2)
-        print(f'{self.nome} aplicou um {g3} e {resultado2}')
+        if resultado2 == 'ACERTOU':
+            self.ponto2 += 1
+        print(f'{self.nome2} aplicou um {g3} e {resultado2}')
+
+    def Final(self):
+        print (f'{self.nome1} fez {self.ponto1} ponto(s).')
+        print (f'{self.nome2} fez {self.ponto2} ponto(s).')
+
+        if self.ponto1 > self.ponto2:
+            print(f'{self.nome1} GANHOU.')
+        elif self.ponto1 < self.ponto2:
+            print(f'{self.nome2} GANHOU.')
+        else:
+            print('A luta foi EMPATADA')
+
+
+    
+
+
+
+        
+
+
+
 """
 class LutadorComputador:
     def __init__(self,*n):
--- principal.py
+++ principal.py
@@ -4,9 +4,14 @@ j1 = Lutador()
 j1.setNome()
 j2 = Lutador()
 j2.setNome2()
-while True:
+
+for r in range(0,5):
     j1.Golpe1()
     j2.Golpe2()
+
+j1.Final()
+
+
 """
 j3 = LutadorComputador()
 j3.setNome2()
