d7c56fbd5d980c8c79df300d72630160af88b28c
Bruno Virgilio de Lima <bvdelima@gmail.com>
2020-09-07 14:20:15 -0300

    parte4-v1

--- modulo.py
+++ modulo.py
@@ -9,7 +9,7 @@ class Luta:
         self.ponto2 = p2
         
     def definirOponente(self):
-        print('''        
+        print('''\033[1;31m        
         -------------------------------------------------        
         -------------------------------------------------        
         -----+++---+++---++++++++++++------+++++---------
@@ -22,7 +22,7 @@ class Luta:
         -----+++++++++---++++--------------+++++---------
         -------------------------------------------------
         -------------------------------------------------
-        ''')        
+        \033[m''')        
         j1=int(input('''\n
         Escolha seu lutador: 
         1 - ANDERSON SILVA
@@ -45,15 +45,15 @@ class Luta:
 
     def IniciarLuta(self):
         for r in range(1,6):
-            print(F'\nROUND {r}')
+            print(f'\n\033[1;33mROUND {r}\033[m')
             g1 = int(input('''
             Sua vez de jogar, escolha seu golpe:
             1 - SOCO
             2 - CHUTE
             Digite Aqui>>> '''))
-            r = ['ACERTOU','ERROU']
+            r = ['\033[1;32mACERTOU\033[m','\033[1;31mERROU\033[m']
             resultado = random.choice(r)
-            if resultado == 'ACERTOU':
+            if resultado == '\033[1;32mACERTOU\033[m':
                 self.ponto1 += 1
             if g1 == 1:
                 print(f'\n{self.nome1} aplicou um SOCO e {resultado}')
@@ -63,9 +63,9 @@ class Luta:
             print('\nComputador vai jogar...'),time.sleep(4)
             g2 = ['SOCO','CHUTE']
             g3 = random.choice(g2)
-            r2 = ['ACERTOU','ERROU']
+            r2 = ['\033[1;32mACERTOU\033[m','\033[1;31mERROU\033[m']
             resultado2 = random.choice(r2)
-            if resultado2 == 'ACERTOU':
+            if resultado2 == '\033[1;32mACERTOU\033[m':
                 self.ponto2 += 1
             print(f'\n{self.nome2} aplicou um {g3} e {resultado2}')
 
@@ -73,7 +73,8 @@ class Luta:
         print('\n|','='*50,'|')
         print('|',' '*20,'RESULTADO',' '*19,'|')
         print('|','='*50,'|')
-        print(f'|{self.nome1} fez {self.ponto1} ponto(s)|')
+        '''print(f'|{self.nome1} fez {self.ponto1} ponto(s)|')'''
+        print('|',' '*20,f'{self.nome1} fez {self.ponto1} ponto(s)',' '*19,'|')
         print (f'|{self.nome2} fez {self.ponto2} ponto(s)|')
         print('|','='*50,'|')
         if self.ponto1 > self.ponto2:
@@ -85,36 +86,6 @@ class Luta:
         else:
             print('|A luta foi EMPATADA|')
             print('|','='*50,'|')
-
-
-    
-
-
-
-        
-
-
-
-"""
-class LutadorComputador:
-    def __init__(self,*n):
-        self.nome = n
-        
-    def setNome2(self):
-        l = ['Jon Jones','George St-Pierre','Chuck Liddell']
-        self.nome = random.choice(l)
-        print(f' O computador escolheu o {self.nome}.')
-        inicio = input('Tecle ENTER para iniciar a luta.')
-
-    def Golpe2(self):
-        print('Computador vai jogar...'),time.sleep(4)
-        g2 = ['SOCO','CHUTE']
-        g3 = random.choice(g2)
-        r2 = ['ACERTOU','ERROU']
-        resultado2 = random.choice(r2)
-        print(f'{self.nome} aplicou um {g3} e {resultado2}')
-"""
-            
 
 
 
