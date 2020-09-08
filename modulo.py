eb5e662d8820ee8bc3fa0f792f7df4fa2b26ed31
Bruno Virgilio de Lima <bvdelima@gmail.com>
2020-09-08 20:13:44 -0300

    finalizando parte 4

--- modulo.py
+++ modulo.py
@@ -22,19 +22,22 @@ class Luta:
         -----+++++++++---++++--------------+++++---------
         -------------------------------------------------
         -------------------------------------------------
-        \033[m''')        
-        j1=int(input('''\n
-        Escolha seu lutador: 
-        1 - ANDERSON SILVA
-        2 - JOSÉ ALDO
-        3 - JUNIOR CIGANO
-        Digite Aqui>>> '''))
-        if j1 == 1:
-            self.nome1 = 'ANDERSON SILVA'    
-        elif j1 == 2:
-            self.nome1 = 'JOSÉ ALDO'            
-        elif j1 == 3:
-            self.nome1 = 'JUNIOR CIGANO'
+        \033[m''')
+        while True:        
+            j1=int(input('''\n
+            Escolha seu lutador: 
+            1 - ANDERSON SILVA
+            2 - JOSÉ ALDO
+            3 - JUNIOR CIGANO
+            Digite Aqui>>> '''))
+            if j1 == 1:
+                self.nome1 = 'ANDERSON SILVA'    
+            elif j1 == 2:
+                self.nome1 = 'JOSÉ ALDO'            
+            elif j1 == 3:
+                self.nome1 = 'JUNIOR CIGANO'
+            else:
+                print('\tESCOLHA INVÁLIDA. DIGITE 1, 2 OU 3')    
 
         print(f'\nSeu lutador é {self.nome1}.')
 
@@ -46,20 +49,25 @@ class Luta:
     def IniciarLuta(self):
         for r in range(1,6):
             print(f'\n\033[1;33mROUND {r}\033[m')
-            g1 = int(input('''
-            Sua vez de jogar, escolha seu golpe:
-            1 - SOCO
-            2 - CHUTE
-            Digite Aqui>>> '''))
-            r = ['\033[1;32mACERTOU\033[m','\033[1;31mERROU\033[m']
-            resultado = random.choice(r)
-            if resultado == '\033[1;32mACERTOU\033[m':
-                self.ponto1 += 1
-            if g1 == 1:
-                print(f'\n{self.nome1} aplicou um SOCO e {resultado}')
-            else:
-                print(f'\n{self.nome1} aplicou um CHUTE e {resultado}')
-
+            while True:
+                g1 = int(input('''
+                Sua vez de jogar, escolha seu golpe:
+                1 - SOCO
+                2 - CHUTE
+                Digite Aqui>>> '''))
+                r = ['\033[1;32mACERTOU\033[m','\033[1;31mERROU\033[m']
+                resultado = random.choice(r)
+                if resultado == '\033[1;32mACERTOU\033[m':
+                    self.ponto1 += 1
+                if g1 == 1:
+                    print(f'\n{self.nome1} aplicou um SOCO e {resultado}')
+                    break
+                elif g1 == 2:
+                    print(f'\n{self.nome1} aplicou um CHUTE e {resultado}')
+                    break
+                else:
+                    print('\t\tESCOLHA INVÁLIDA. DIGITE 1 OU 2')
+                
             print('\nComputador vai jogar...'),time.sleep(4)
             g2 = ['SOCO','CHUTE']
             g3 = random.choice(g2)
@@ -73,19 +81,16 @@ class Luta:
         print('\n|','='*50,'|')
         print('|',' '*20,'RESULTADO',' '*19,'|')
         print('|','='*50,'|')
-        '''print(f'|{self.nome1} fez {self.ponto1} ponto(s)|')'''
-        print('|',' '*20,f'{self.nome1} fez {self.ponto1} ponto(s)',' '*19,'|')
-        print (f'|{self.nome2} fez {self.ponto2} ponto(s)|')
+        print(' '*11,f'{self.nome1} fez {self.ponto1} ponto(s)',' '*14)
+        print(' '*11,f'{self.nome2} fez {self.ponto2} ponto(s)',' '*14)
         print('|','='*50,'|')
         if self.ponto1 > self.ponto2:
-            print(f'|{self.nome1} GANHOU|')
-            print('|','='*50,'|')
+            print('+'*14,f'{self.nome1:} GANHOU','+'*14)
         elif self.ponto1 < self.ponto2:
-            print(f'|{self.nome2} GANHOU|')
-            print('|','='*50,'|')
+            print('+'*14,f'{self.nome2:} GANHOU','+'*14)
         else:
-            print('|A luta foi EMPATADA|')
-            print('|','='*50,'|')
+            print(  '+'*15,'A luta foi EMPATADA','+'*15)
+        print('|','='*50,'|')
 
 
 
